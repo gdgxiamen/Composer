5248f32c6b2a2451873c5814ce9f56ec59a33afe
Wind Wood <windwood@gmail.com>
2015-10-06 07:24:52 +0800

    a app layout without canvas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  var app = document.querySelector('x-app');
 
   app.displayInstalledToast = function() {
     // Check to make sure caching is actually enabled—it won't be in the dev environment.
